cb31f8e687552b01575120bdb10bbe92b5a70dbb
tnikFi <95296580+tnikFi@users.noreply.github.com>
2024-04-04 02:57:23 +0300

    Refactor action error handling

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,13 @@ import { glob } from 'glob'
 import { parseReplacements, transformJsonFile } from './tokenReplacement'
 
 const transformFile = async (file: string, replacements: Record<string, string>) => {
-  try {
-    core.info(`Transforming file: ${file}`)
-    const transformedKeys = transformJsonFile(file, file, replacements)
-    if (transformedKeys.length > 0) {
-      core.info(transformedKeys.map((key) => `\tReplaced key: ${key}`).join('\n'))
-      core.info(`${transformedKeys.length} key(s) replaced in file: ${file}`)
-    } else {
-      core.warning(`No keys replaced in file: ${file}`)
-    }
-  } catch (error) {
-    core.error(`Error transforming file: ${file}`)
-    core.error(error as Error)
+  core.info(`Transforming file: ${file}`)
+  const transformedKeys = transformJsonFile(file, file, replacements)
+  if (transformedKeys.length > 0) {
+    core.info(transformedKeys.map((key) => `\tReplaced key: ${key}`).join('\n'))
+    core.info(`${transformedKeys.length} key(s) replaced in file: ${file}`)
+  } else {
+    core.warning(`No keys replaced in file: ${file}`)
   }
 }
 
@@ -24,7 +19,13 @@ const action = async () => {
   const pattern = core.getInput('files')
   const files = await glob(pattern)
   for (const file of files) {
-    transformFile(file, replacements)
+    try {
+      transformFile(file, replacements)
+    } catch (error) {
+      core.error(`Error transforming file: ${file}`)
+      core.error(error as Error)
+      throw error
+    }
   }
 }
 
